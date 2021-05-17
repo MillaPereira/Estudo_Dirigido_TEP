16c7bd75943e182743db53a3323320e1066927e4
Lara Tagarro Bernardes <74369384+Laratb1@users.noreply.github.com>
2021-05-17 12:05:07 -0300

    Add files via upload

--- source/double_t.h
+++ source/double_t.h
@@ -1,1 +1,10 @@
+#ifndef DOUBLE_H
+#define DOUBLE_H
 
+#undef NATIVO_
+#undef PREF_
+#define NATIVO_ double
+#define PREF_(COISA) Double_ ## COISA
+#include "converteNativo.h"
+
+#endif
--- source/tiposFuncoes.h
+++ source/tiposFuncoes.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* typedef do malloc */
+typedef void * (* func1_1size_t ) (size_t tamanho);
+
+/* typedef do memcpy */
+typedef void * (* func1_2void_1size_t) (void * restrict um, const void * restrict dois, unsigned int tamanho);
+
+/* typedef do free */
+typedef void (* func0_1void_t) (void *um);
 
